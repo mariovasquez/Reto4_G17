bcdb56025f95b4e486812ff6a653f0cedd28e53c
mariovasquez <N00156666@upn.pe>
2022-09-11 02:37:47 -0500

    Update hero slider swiper js

--- js/index.js
+++ js/index.js
@@ -3,12 +3,14 @@ import counter from "./modules/counter.js";
 import header from "./modules/header.js";
 import switcher from "./modules/switcher.js";
 import featured from "./modules/featured.js";
+import slider from "./modules/slider.js";
 
 const documentReady = () => {
   counter();
   header();
   switcher();
   featured();
+  slider();
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
